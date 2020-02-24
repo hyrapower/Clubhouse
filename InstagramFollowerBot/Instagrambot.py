2d22121d3b0fcf412746cd64b88cb9832597673a
lawfull26 <law_-2@outlook.com>
2020-02-23 22:21:20 -0500

    Hooked instagram bot to the php file and makes it run infinitely along with refresh behavior and clean up on failure

---
+++ InstagramFollowerBot/Instagrambot.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from time import sleep
+
+
+# this bot was created by AJ-4, original script located here
+# tinderbot rquires you to run this script in version 2.7
+# please run py -2.7 tinderbot.py in the console to run this script if you need to
+# change the credentials in secrets.py in order to run the application
+# you need to run virtualenv venv to start server
+class InstagramBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+    def getInstagramPage(self):
+        while True:
+            try:
+                self.driver.get('http://localhost/?user=hardlydoinit')
+
+                sleep(6)
+
+                followerCount = self.driver.find_element_by_xpath('/html/body/div/div/div[2]/div[1]/div[2]/span').text
+                print (followerCount)
+
+                sleep(600)
+                
+                self.driver.navigate().refresh()
+            except Exception:
+                break
+
+    def exitDriver(self):
+        self.driver.quit()
+
+    def closeDriver(self):
+        self.driver.close()
+
+bot = InstagramBot()
+
+while True:
+    try:
+        bot.getInstagramPage()
+    except Exception:
+        print ('login failed')
+        bot.exitDriver()
+        break
--- InstagramFollowerBot/index.php
+++ InstagramFollowerBot/index.php
@@ -1,4 +1,3 @@
-<meta http-equiv="refresh" content="600" >
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <style>
